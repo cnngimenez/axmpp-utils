0701b08be007db54a90379a3c2b5f11a0ef5853e
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2021-06-20 20:25:07 -0300

    Removing hardcoding tests. Using File_Info.

--- source/send_file/send_file_handlers.adb
+++ source/send_file/send_file_handlers.adb
@@ -6,12 +6,12 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2011, Alexander Basov <coopht@gmail.com>                     --
--- All rights reserved.                                                     --
+--  Copyright © 2011, Alexander Basov <coopht@gmail.com>                     --
+--  All rights reserved.                                                     --
 --                                                                          --
--- Redistribution and use in source and binary forms, with or without       --
--- modification, are permitted provided that the following conditions       --
--- are met:                                                                 --
+--  Redistribution and use in source and binary forms, with or without       --
+--  modification, are permitted provided that the following conditions       --
+--  are met:                                                                 --
 --                                                                          --
 --  * Redistributions of source code must retain the above copyright        --
 --    notice, this list of conditions and the following disclaimer.         --
@@ -24,17 +24,17 @@
 --    contributors may be used to endorse or promote products derived from  --
 --    this software without specific prior written permission.              --
 --                                                                          --
--- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
--- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
--- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
--- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
--- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
--- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
--- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
--- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
--- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
--- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
--- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+--  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+--  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+--  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+--  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+--  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+--  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+--  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+--  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+--  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+--  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
 --                                                                          --
 ------------------------------------------------------------------------------
 --  $Revision$ $Date$
@@ -253,12 +253,9 @@ package body Send_File_Handlers is
            Request.Set_To (To_Universal_String ("upload.") & Self.Config.Host);
            Request.Set_From (Self.Config.JID);
 
-           --  TODO: Obtain from the file itself.
-           Request.Set_Filename (To_Universal_String ("turing-small-min.jpg"));
-           Request.Set_Size
-             (To_Universal_String
-                (Integer'Wide_Wide_Image (1785)));
-           Request.Set_Content_Type (To_Universal_String ("image/jpeg"));
+           Request.Set_Filename (Self.File_Info.Get_Name);
+           Request.Set_Size (Self.File_Info.Get_Size);
+           Request.Set_Content_Type (Self.File_Info.Get_Content_Type);
 
            Self.Object.Send_Object (Request);
 
